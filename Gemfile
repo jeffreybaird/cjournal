ff20aee29a86f567d055af6e948455356b777ef0
jeffreybaird <jbaird@sweetspotdiabetes.com>
2014-01-03 22:28:39 -0800

    initial commit

---
+++ Gemfile
@@ -0,0 +1,15 @@
+source "https://rubygems.org"
+
+
+gem "sinatra"
+gem "sqlite3"
+gem "activerecord"
+gem "sinatra-activerecord"
+gem "rake"
+gem "haml"
+gem "sass"
+
+group :development do
+  gem "shotgun"
+  gem "tux"
+end
---
+++ app.rb
@@ -0,0 +1,39 @@
+require 'bundler'
+Bundler.require
+
+Dir.glob('./lib/*.rb') do |model|
+  require model
+end
+
+
+module Cjournal
+
+  class App < Sinatra::Application
+    require 'sass/plugin/rack'
+    use Sass::Plugin::Rack
+
+    #configure
+    configure do
+      set :root, File.dirname(__FILE__)
+      set :public_folder, 'public'
+    end
+
+    #database
+    set :database, "sqlite3:///cjournal.db"
+
+    #filters
+
+    #routes
+    get '/' do
+      haml :index
+    end
+
+    #helpers
+    helpers do
+      def partial(file_name)
+        erb file_name, :layout => false
+      end
+    end
+
+  end
+end
